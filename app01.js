1588f6bc33f72b265173e181d1159af70d45d86f
AlonBenAriVA <37850639+AlonBenAriVA@users.noreply.github.com>
2018-07-19 14:58:37 -0700

    Add files via upload

--- app01.js
+++ app01.js
@@ -21,7 +21,7 @@ const main = async () => {
     FROM [CDWWork].[Dim].[Date]`;
    */
     const query0 = 
-    'SELECT TOP (1000) * FROM [D05_VISN21Sites].[LSV].[MAC_ACSC]'
+    'SELECT * FROM [D05_VISN21Sites].[LSV].[MAC_ACSC]'
 
     const result = await request.query(query0);
   
@@ -30,7 +30,7 @@ const main = async () => {
     return result.recordsets[0];
     
   };
-//app.use(express.static(__dirname))
+app.use(express.static(__dirname))
 app.use(logger("short"))
 var publicPath = path.resolve(__dirname,'public')
 app.use(express.static(publicPath))
@@ -44,7 +44,7 @@ app.get('/GetData',function(req,res){
     a.then(function(r) {
         console.log(r)
         console.log('Here we go')
-        res.json(r)
+        res.status(200).json(r)
         res.end
     }, function(err) {
         console.log('fetch error',err)
@@ -60,10 +60,11 @@ app.get('/GetData',function(req,res){
 })
 
 app.get('/data', function(req, res){
- res.json([{
-     id:1,name:'Alon',
-     id:2,name: 'Elvis'
- }])
+    //res.setHeader('')
+ res.send(JSON.stringify({
+     "id":1,name:'Alon',
+     "id":2,name: 'Elvis'
+ }))
 })
 app.get('/circle',function(req,res){
     res.send('<html><body><h1>My first SVG</h1><svg width="100" height = "100"> <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></body></html>');
